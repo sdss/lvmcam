3c404b59576dd422f5604ff341c1efe9fd446b31
mgjeon <mgjeon@khu.ac.kr>
2021-09-15 12:26:33 +0900

    RTD test

--- python/lvmcam/araviscam/aravis.py
+++ python/lvmcam/araviscam/aravis.py
@@ -1,5 +1,5 @@
-# import gi
+import gi
 # gi.require_version('Aravis', '0.8')
-# from gi.repository import Aravis
-class Aravis():
-    pass
+from gi.repository import Aravis
+# class Aravis():
+    # pass
