7361e30b8ee1c7684750008bfcbb32c7e458c6f8
Florian Briegel <briegel@ogre.fritz.box>
2022-07-22 00:21:47 +0200

    fixed last remaining of config_get

--- python/lvmcam/actor/actor.py
+++ python/lvmcam/actor/actor.py
@@ -83,7 +83,7 @@ class LvmcamActor(BaseCameraActor, AMQPActor):
             self.log.sh.setLevel(DEBUG)
             self.log.sh.formatter = StreamFormatter(fmt='%(asctime)s %(name)s %(levelname)s %(filename)s:%(lineno)d: \033[1m%(message)s\033[21m') 
 
-        self.dirname = config_get(config, "dirname", None)
+        self.dirname = config.get("dirname", None)
         self.basename = config.get("basename", None)
 
         self.exposure_state = {}
