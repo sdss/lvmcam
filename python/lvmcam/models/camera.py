6412d4d1a3ab898a269808417b671f2bd365f62b
José Sánchez-Gallego <gallegoj@uw.edu>
2023-07-03 13:29:25 -0700

    Missing }

--- python/lvmcam/models/camera.py
+++ python/lvmcam/models/camera.py
@@ -15,7 +15,7 @@ CameraCards = CardGroup(
         Card("OBSERVAT", value="LCO", comment="Observatory"),
         Card(
             "TELESCOP",
-            value="{__exposure__.camera.telescope",
+            value="{__exposure__.camera.telescope}",
             comment="Telescope that took the image",
         ),
         Card("INSTRUME", value="LVM", comment="SDSS-V Local Volume Mapper"),
